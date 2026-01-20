a12d6aa0e4691990bb51765fe93ade18da4410a5
ihwan <ihwan.bpafk@gmail.com>
2026-01-20 09:42:11 +0700

    feat : fix login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,11 +8,11 @@ use Illuminate\Support\Facades\Auth;
 class AuthController extends Controller
 {
     public function showLogin() {
-        $request->session()->regenerate();
         return view('auth.login');
     }
 
     public function login(Request $request) {
+        $request->session()->regenerate();
         $credentials = $request->validate([
             'username' => 'required',
             'password' => 'required'
